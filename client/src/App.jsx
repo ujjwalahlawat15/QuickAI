5e05968fa4b09e199a271a88c6eaa479b105e3a4
ujjwalahlawat15 <ujjwalahlawat044gmail.com>
2025-07-24 04:57:13 +0530

    project completed

---
+++ client/src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Layout from './pages/Layout'
+import Dashboard from './pages/Dashboard'
+import WriteArticle from './pages/WriteArticle'
+import BlogTitles from './pages/BlogTitles'
+import GenerateImages from './pages/GenerateImages'
+import RemoveBackground from './pages/RemoveBackground'
+import RemoveObjects from './pages/RemoveObjects'
+import ReviewResume from './pages/ReviewResume'
+import Community from './pages/Community'
+import { useAuth } from '@clerk/clerk-react'
+
+const App = () => {
+
+  const {getToken} = useAuth()
+  useEffect(()=>{
+    getToken().then((token)=>console.log(token));
+  })
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/ai' element={<Layout />}>
+        <Route index element={<Dashboard/>}/>
+        <Route path='write-article' element={<WriteArticle/>} />
+        <Route path='blog-titles' element={<BlogTitles/>} />
+        <Route path='generate-images' element={<GenerateImages/>} />
+        <Route path='remove-background' element={<RemoveBackground/>} />
+        <Route path='remove-object' element={<RemoveObjects/>} />
+        <Route path='review-resume' element={<ReviewResume/>} />
+        <Route path='community' element={<Community/>} />
+
+
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
